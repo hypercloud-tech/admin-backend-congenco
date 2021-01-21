0fb7f2046764b66dcc21daec38daf9f87b5b6df7
Headsabovethecloud <58742683+Headsabovethecloud@users.noreply.github.com>
2021-01-21 10:02:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-pm2 kill
-pm2 start npm --name "prod-admin-backend" -- start
+/home/ec2-user/.nvm/versions/node/v9.3.0/bin/pm2 kill
+/home/ec2-user/.nvm/versions/node/v9.3.0/bin/pm2 start npm --name "prod-admin-backend" -- start
